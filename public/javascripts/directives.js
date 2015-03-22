c68178393a8694b16b823b0d671a24538329c25f
justincoh <justincoh@gmail.com>
2015-03-22 18:38:17 -0400

    Area graph in place too, commented out

--- public/javascripts/directives.js
+++ public/javascripts/directives.js
@@ -20,81 +20,103 @@ app.directive('summaryTable', function() {
         },
         link: function(scope, element, attrs) {
             scope.buildChart = function(data) {
-                    var margin = {
-                            top: 20,
-                            right: 20,
-                            bottom: 30,
-                            left: 75
-                        },
-                        width = 960 - margin.left - margin.right,
-                        height = 500 - margin.top - margin.bottom;
+                var margin = {
+                        top: 20,
+                        right: 20,
+                        bottom: 30,
+                        left: 75
+                    },
+                    width = 960 - margin.left - margin.right,
+                    height = 500 - margin.top - margin.bottom;
 
-                    var parseDate = d3.time.format("%Y-%m-%d"); //.parse
+                var parseDate = d3.time.format("%Y-%m-%d"); //.parse
 
-                    var x = d3.time.scale()
-                        .range([0, width])
+                var x = d3.time.scale()
+                    .range([0, width])
 
-                    var y = d3.scale.linear()
-                        .range([height, 0]);
+                var y = d3.scale.linear()
+                    .range([height, 0]);
 
-                    var xAxis = d3.svg.axis()
-                        .scale(x)
-                        .orient("bottom")
-                        .ticks(d3.time.day, 1)
-                        .tickFormat(d3.time.format("%Y-%m-%d"));
+                var xAxis = d3.svg.axis()
+                    .scale(x)
+                    .orient("bottom")
+                    .ticks(d3.time.day, 1)
+                    .tickFormat(d3.time.format("%Y-%m-%d"));
 
-                    var yAxis = d3.svg.axis()
-                        .scale(y)
-                        .orient("left");
+                var yAxis = d3.svg.axis()
+                    .scale(y)
+                    .orient("left");
 
-                    var line = d3.svg.line()
-                        .x(function(d) {
-                            // console.log('x(d ',x(d.date), x)
-                            return x(d.date);
-                        })
-                        .y(function(d) {
-                            return y(d.downloads);
-                        });
+                // var area = d3.svg.area()
+                //     .x(function(d) {
+                //         return x(d.date);
+                //     })
+                //     .y0(height)
+                //     .y1(function(d) {
+                //         return y(d.downloads);
+                //     });
 
-                    var svg = d3.select("#chart-container").append("svg")
-                        .attr("width", width + margin.left + margin.right)
-                        .attr("height", height + margin.top + margin.bottom)
-                        .append("g")
-                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-                    var data = scope.summaryData.downloads
-                    data.forEach(function(d) {
-                        d.date = new Date(d.date);
+                var line = d3.svg.line()
+                    .x(function(d) {
+                        // console.log('x(d ',x(d.date), x)
+                        return x(d.date);
+                    })
+                    .y(function(d) {
+                        return y(d.downloads);
                     });
 
-                    x.domain(d3.extent(data, function(d) {
-                        console.log('in domain ', parseDate(d.date))
-                        return d.date;
-                    }));
-                    y.domain(d3.extent(data, function(d) {
-                        return d.downloads;
-                    }));
+                var svg = d3.select("#chart-container").append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                    .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-                    svg.append("g")
-                        .attr("class", "x axis")
-                        .attr("transform", "translate(0," + height + ")")
-                        .call(xAxis);
+                var data = scope.summaryData.downloads
+                data.forEach(function(d) {
+                    d.date = new Date(d.date);
+                });
 
-                    svg.append("g")
-                        .attr("class", "y axis")
-                        .call(yAxis)
-                        .append("text")
-                        .attr("transform", "rotate(-90)")
-                        .attr("y", 6)
-                        .attr("dy", ".71em")
-                        .style("text-anchor", "end")
-                        .text("Downloads");
+                x.domain(d3.extent(data, function(d) {
+                    console.log('in domain ', parseDate(d.date))
+                    return d.date;
+                }));
 
-                    svg.append("path")
-                        .datum(data)
-                        .attr("class", "line")
-                        .attr("d", line);
-                }
+                var downloadMax = d3.max(data,function(d){
+                    return d.downloads;
+                })
+                var downloadMin = d3.min(data,function(d){
+                    return d.downloads;
+                })
+                y.domain([0,downloadMax*1.2])
+                // y.domain(d3.extent(data, function(d) {
+                //     return d.downloads;
+                // }));
+
+                svg.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(xAxis);
+
+                svg.append("g")
+                    .attr("class", "y axis")
+                    .call(yAxis)
+                    .append("text")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 6)
+                    .attr("dy", ".71em")
+                    .style("text-anchor", "end")
+                    .text("Downloads");
+
+                svg.append("path")
+                    .datum(data)
+                    .attr("class", "line")
+                    .attr("d", line);
+
+                // svg.append("path")
+                //     .datum(data)
+                //     .attr("class", "area")
+                //     .attr("d", area);
+            }
         }
     }
 })
