2d7f4c84d2fd56614876cbf2f46a76207054e6f9
justincoh <justincoh@gmail.com>
2015-03-25 13:20:07 -0400

    changed error handling code

--- app.js
+++ app.js
@@ -50,10 +50,11 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    // res.render('error', {
+    //     message: err.message,
+    //     error: {}
+    // });
+    return res.json({message: err.message,error:err})
 });
 
 
